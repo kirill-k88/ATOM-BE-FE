4c208d8b4016df4edeeeb0265311e4cab015a7d2
kirill-k88 <kirill-k88@ya.ru>
2024-04-26 20:07:46 +0300

    Добавил скрипты для получения данных

--- utils/constants/db_connection.js
+++ utils/constants/db_connection.js
@@ -3,3 +3,6 @@ module.exports.DB_HOST = 'localhost';
 module.exports.DB = 'atom_db';
 module.exports.DB_PASSWORD = '12345678';
 module.exports.DB_PORT = 5432;
+
+module.exports.DB_INSERT_ERROR = 'Ошибка при ввполнении запроса на добавление в БД.';
+module.exports.DB_SELECT_ERROR = 'Ошибка при ввполнении запроса на чтение из БД.';
